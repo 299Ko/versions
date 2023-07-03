f10999b5d2ee1f8780388eb454e7734cb7d3c181
Max Koder <mx.koder@gmail.com>
2023-07-03 19:31:41 +0200

    V 1.2.3

--- execUpdate.php
+++ execUpdate.php
@@ -17,9 +17,9 @@
 define('KOPATH', '../299ko/');
 define('VERSIONSPATH', '../versions/');
 
-$version = '1.2.2';
-$commitLastVersion = 'a6498257e52d520ba32b6b9ce6a152cd00fa11ea';
-$commitFutureVersion = '2c65b43c3c0cbed2bd40fe9675d5a606257b0857';
+$version = '1.2.3';
+$commitLastVersion = '2c65b43c3c0cbed2bd40fe9675d5a606257b0857';
+$commitFutureVersion = '00ff2f26d19aa2091c625480a75547e5feba9d69';
 
 chdir(KOPATH);
 
