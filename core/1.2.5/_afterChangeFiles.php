9752ae9ca859ac7dca0460bab03749f4eb96bd40
Max Koder <mx.koder@gmail.com>
2023-07-08 15:55:45 +0200

    V 1.2.5

---
+++ core/1.2.5/_afterChangeFiles.php
@@ -0,0 +1,19 @@
+<?php
+
+logg("Began of _afterChangeFiles", "INFO");
+
+$blogConfig = util::readJsonFile(DATA_PLUGIN . 'blog/config.json');
+
+$blogConfig["authorName"] ??= "";
+$blogConfig["authorAvatar"] ??= "";
+$blogConfig["authorBio"] ??= "";
+$blogConfig["displayAuthor"] ??= false;
+
+if (util::writeJsonFile(DATA_PLUGIN . 'blog/config.json', $blogConfig)) {
+    logg("Blog Config changed for Author", "INFO");
+} else {
+    logg("Unable to modify blog config", "ERROR");
+}
+
+logg("End of _afterChangeFiles", "INFO");
+
--- execUpdate.php
+++ execUpdate.php
@@ -17,9 +17,9 @@
 define('KOPATH', '../299ko/');
 define('VERSIONSPATH', '../versions/');
 
-$version = '1.2.4';
-$commitLastVersion = '00ff2f26d19aa2091c625480a75547e5feba9d69';
-$commitFutureVersion = 'd113fd6a065f43f86dc6f1bed01ceb0889792221';
+$version = '1.2.5';
+$commitLastVersion = 'd113fd6a065f43f86dc6f1bed01ceb0889792221';
+$commitFutureVersion = 'c2645998459836e4532a64f5af8ef6d1bba37859';
 
 chdir(KOPATH);
 
