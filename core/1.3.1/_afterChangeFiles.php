6e7c34bf4caab7e448c74b5268b948a682ccc701
Max Koder <mx.koder@gmail.com>
2023-10-01 16:13:06 +0200

    FIX 1.3.1...

--- core/1.3.1/_afterChangeFiles.php
+++ core/1.3.1/_afterChangeFiles.php
@@ -4,6 +4,10 @@ logg("Began of _afterChangeFiles", "INFO");
 
 // After blog change config file
 pluginsManager::getInstance()->installPlugin('blog', true);
+pluginsManager::getInstance()->installPlugin('blog', true);
+
+pluginsManager::getInstance()->installPlugin('seo', true);
+pluginsManager::getInstance()->installPlugin('seo', true);
 
 logg("End of _afterChangeFiles", "INFO");
 return true;
