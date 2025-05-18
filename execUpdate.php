1c9c51f73540072ca9b1736f61fbcd1787cb6029
Max Koder <mx.koder@gmail.com>
2025-05-18 15:30:09 +0200

    Improve for manual update

--- execUpdate.php
+++ execUpdate.php
@@ -1,25 +1,18 @@
 <?php
 
 /**
- * @copyright (C) 2022, 299Ko, based on code (2010-2021) 99ko https://github.com/99kocms/
+ * @copyright (C) 2025, 299Ko, based on code (2010-2021) 99ko https://github.com/99kocms/
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
- * @author Jonathan Coulet <j.coulet@gmail.com>
  * @author Maxence Cauderlier <mx.koder@gmail.com>
- * @author Frédéric Kaplon <frederic.kaplon@me.com>
- * @author Florent Fortat <florent.fortat@maxgun.fr>
  * 
  * @package 299Ko https://github.com/299Ko/299ko
  */
 
 // need to be started in CLI, in versions folder
 // php execUpdate.php
+// Create a new version for auto update
 
-define('KOPATH', '../299ko/');
-define('VERSIONSPATH', '../versions/');
-
-$version = '2.0.0';
-$commitLastVersion =   'ac061f240360e5d22b41a236768628cf143e9821';
-$commitFutureVersion = 'aa4cae7fe115aebf8a17b3e6a6bfc325c1f0f2fd';
+require 'config.php';
 
 chdir(KOPATH);
 
@@ -27,16 +20,22 @@ $result = null;
 exec('git diff --name-status ' . $commitLastVersion . ' ' . $commitFutureVersion, $result);
 
 chdir(VERSIONSPATH);
+@mkdir('update_to/' . $version);
 
+$deleted = [];
 $json = [];
 
 foreach ($result as $r) {
     $line = explode("\t", $r);
     if (count($line) === 2) {
         $json[$line[0]][] = $line[1];
+        if ($line[0] === 'D') {
+            $deleted[] = $line[1];
+        }
     } elseif (count($line) === 3) {
         if (substr($line[0],0,1) === "R") {
             // Rename detected
+            $deleted[] = $line[1];
             $json["D"][] = $line[1];
             $json["A"][] = $line[2];
         }
@@ -47,5 +46,5 @@ if (!is_dir('core/' . $version)) {
     mkdir('core/' . $version, 0777, true);
 }
 
-file_put_contents('core/' . $version . '/files.json', json_encode($json, true));
-
+file_put_contents('core/' . $version . '/files.json', json_encode($json, JSON_PRETTY_PRINT));
+file_put_contents('core/' . $version . '/deleted.json', json_encode($deleted, JSON_PRETTY_PRINT));
